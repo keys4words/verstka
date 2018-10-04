1ed29f3c401c5514aa4f04ba5c64c97615b9c62d
zharnikov <zharnikov@mbgbeauty.ru>
2018-10-04 18:19:15 +0300

    modify form and add practise with css animation in css

--- form.php
+++ form.php
@@ -7,20 +7,20 @@
 		$phone = trim($_POST['phone']);
 		
 		//валидация введенных в форму данных
-		if(false){
-			
+		if($name == "" || $phone == ""){
+			$msg = 'Заполните все поля';
 		}
-		else if(false){
-			
-			//$msg = 'Ошибка ввода';
+		else if(mb_strlen($name, 'UTF-8') < 2){
+			$msg = 'Имя не короче 2 символов';
+		}
+		else if(!ctype_digit($phone)){
+			$msg = 'Телефон только из цифр';
 		}
 		else{
 			file_put_contents('apps.txt', "$dt $name $phone", FILE_APPEND);
 			mail('admin@localhost.ru', 'New order', "$dt $name $phone");
 			$msg = 'Заявка принята! Ждите!!!';
 		}
-		
-		
 		
 	}else{
 		//просто зашел на страничку
@@ -30,7 +30,8 @@
 
 ?>
 
-
+<?php 
+	if(true) {?>
 <form method="post" action="">
 	Имя<br>
 	<input type="text" name="name"><br>
@@ -39,6 +40,7 @@
 	<button>Заказать звонок</button>
 
 </form>
+	<?php }?>
 
 <div>
 	<?php echo $msg; ?>
