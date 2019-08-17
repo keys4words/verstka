52061827b9238859b97c8a68bdc3905035b9b388
Max 19038 <keys4words@gmail.com>
2019-08-17 23:00:22 +0300

    add js intro

---
+++ webdevbootcamp/js/js-intro/script.js
@@ -0,0 +1,5 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+alert("Nice to meet you, " + userFirstName + " " + userLastName);
+console.log("Your age is " + userAge*365)
