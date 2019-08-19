e901289913a5b428aa96e9fa3bc8aea1c4e3cd02
keys4words <keys4words@gmail.com>
2019-08-19 17:01:45 +0300

    add while loops

---
+++ webdevbootcamp/js/annoy/annoy.js
@@ -0,0 +1,12 @@
+// var answer = prompt('Are we there yet?');
+// while(answer!=='yes' && answer !=='yeah'){
+//     var answer = prompt('Are we there yet?');
+// }
+// alert('YAY, we made it');
+
+
+var answer = prompt('Are we there yet?');
+while(answer.indexOf('yes') !== -1){
+    var answer = prompt('Are we there yet?');
+}
+alert('YAY, we made it');
---
+++ webdevbootcamp/js/js-intro/loops.js
@@ -0,0 +1,28 @@
+// var counter = -10;
+// while(counter<20){
+//     console.log(counter);
+//     counter++;
+// }
+
+
+// var counter = 10;
+// while(counter<=40){
+//     console.log(counter);
+//     counter+=2;
+// }
+
+// var counter = 300;
+// while(counter<=333){
+//     if(counter % 2 !== 0){
+//         console.log(counter);
+//     }
+//     counter++;
+// }
+
+var counter = 5;
+while(counter<=50){
+    if(counter%5 == 0 && counter%3==0){
+        console.log(counter);
+    }
+    counter++;
+}
