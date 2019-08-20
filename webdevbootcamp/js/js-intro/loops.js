8102f74740a6e79b011a63fb0a3bada232c6f916
keys4words <keys4words@gmail.com>
2019-08-20 15:52:08 +0300

    add for loops

--- webdevbootcamp/js/js-intro/loops.js
+++ webdevbootcamp/js/js-intro/loops.js
@@ -3,13 +3,18 @@
 //     console.log(counter);
 //     counter++;
 // }
-
+// for(var i=-10; i<20; i++){
+//     console.log(i);
+// }
 
 // var counter = 10;
 // while(counter<=40){
 //     console.log(counter);
 //     counter+=2;
 // }
+for(var i=-10; i<=40; i+=2){
+    console.log(i);
+}
 
 // var counter = 300;
 // while(counter<=333){
@@ -19,10 +24,10 @@
 //     counter++;
 // }
 
-var counter = 5;
-while(counter<=50){
-    if(counter%5 == 0 && counter%3==0){
-        console.log(counter);
-    }
-    counter++;
-}
+// var counter = 5;
+// while(counter<=50){
+//     if(counter%5 == 0 && counter%3==0){
+//         console.log(counter);
+//     }
+//     counter++;
+// }
