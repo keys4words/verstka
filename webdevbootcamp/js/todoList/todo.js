face7dddc1baf822faad5816fe07784e526707aa
keys4words <keys4words@gmail.com>
2019-08-23 17:00:13 +0300

    add 131 solution

--- webdevbootcamp/js/todoList/todo.js
+++ webdevbootcamp/js/todoList/todo.js
@@ -1,14 +1,35 @@
 var todos = ['test'];
-var input = prompt('What would like to do?').toLowerCase();
+var input = prompt('What would like to do?');
 
 
 while(input !== 'quit'){
-    if (input === 'list'){
-        console.log(todos);
+    if (input === 'show'){
+        showList();
     } else if(input === 'new'){
-        var newTodo = prompt('Enter new todo:');
-        todos.push(newTodo);
+        addTodo();
+    } else if(input === 'delete'){
+        deleteTodo();
     }
     input = prompt('What would like to do?').toLowerCase();
 }
 console.log('OK, you quit to app');
+
+function showList(){
+    console.log('*********************');
+    todos.forEach(function(todo, i){
+        console.log('#' + i + ": " + todo);
+    });
+    console.log('*********************');
+}
+
+function addTodo(){
+    var newTodo = prompt('Enter new todo:');
+    todos.push(newTodo);
+    console.log('Todo added');
+}
+
+function deleteTodo(){
+    var index = prompt("Enter index of todo to delete:");
+    todos.splice(index, 1);
+    console.log('Todo deleted');
+}
