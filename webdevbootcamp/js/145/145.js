bdf6f87b4391f84fe23caa3d9e1caf02b6af42d5
keys4words <keys4words@gmail.com>
2019-09-11 16:19:49 +0300

    add 145 lesson

---
+++ webdevbootcamp/js/145/145.js
@@ -0,0 +1,8 @@
+var tag = document.getElementById('highlight');
+var tags = document.getElementsByClassName('bolded');
+var tags_type = document.getElementsByTagName('li');
+
+var tag1 = document.querySelector("#highlight");
+var tag2 = document.querySelector(".bolded"); 
+var tag3 = document.querySelectorAll(".bolded");
+
