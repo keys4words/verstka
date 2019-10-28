936431bc5817834c52d743386da2c1c9d3f9ac07
keys4words <keys4words@gmail.com>
2019-10-28 17:10:18 +0300

    add patterns response

--- webdevbootcamp/introToExpress/FirstExpressApp/app.js
+++ webdevbootcamp/introToExpress/FirstExpressApp/app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 
+
+
 // "/" => "hi there!"
 app.get("/", function(req, res){
     res.send("Hi there!");
@@ -15,6 +17,16 @@ app.get("/dog", function(req, res){
     console.log("someone made a request to /dog");
     res.send("GAV!!!");
 });
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res){
+    var subreddit = req.params.subredditName;
+    res.send('Welcome to the ' + subreddit.toUpperCase() + " page");
+});
+
+app.get("*", function(req, res){
+    res.send("You are the STAR");
+});
+
 
 app.listen(3000, function(){
     console.log("Server has running...");
