9138a08e9d75610fa62fa026c5688e2de3888c36
keys4words <keys4words@gmail.com>
2019-11-01 12:08:32 +0300

    add moreExpress example  with loops and ejs

---
+++ webdevbootcamp/moreExpress/EJSDemo/index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+    var thing = req.params.thing;
+    res.render("love.ejs", {thingVar: thing});
+});
+
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "post 1", author: 'Susy'},
+        {title: "My adorable pet bunny", author: 'Santa'},
+        {title: "Can you believe in this story?", author: 'John Doe'}
+    ];
+
+    res.render("posts.ejs", {posts: posts});
+});
+
+app.listen(3000, function(){
+    console.log("Server is listening...");
+});
