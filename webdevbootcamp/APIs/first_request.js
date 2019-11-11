bc259541ca7030017bf026ae8020b26730594a09
keys4words <keys4words@gmail.com>
2019-11-11 14:50:04 +0300

    fix error with api jsonplaceholder

--- webdevbootcamp/APIs/first_request.js
+++ webdevbootcamp/APIs/first_request.js
@@ -1,10 +1,15 @@
 var request = require('request');
 //used this free api https://sunrise-sunset.org/api
 //also may use this api for tests https://jsonplaceholder.typicode.com/
-request('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400', function (error, response, body) {
+//var url = 'https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400';
+var url = 'https://jsonplaceholder.typicode.com/users';
+request(url, function (error, response, body) {
     if(!error && response.statusCode == 200){
         var parsedData = JSON.parse(body);
-        console.log(parsedData["results"]["sunrise"]);
+        //console.log(parsedData["results"]["sunrise"]);
+        parsedData.forEach(element => {
+            console.log(element.name + '\t' + element.email + '\t' + element.phone);
+        });
     } 
 });
 
